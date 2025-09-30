8d982254e4b56d477c58942c6ae5de72d9f86940
Dương Đình Trác - Lab1 <duongdinhtrac0103@gmail.com>
2025-09-30 08:15:29 +0700

    update

---
+++ src/main/java/poly/edu/contronller/Home/HomeController.java
@@ -0,0 +1,14 @@
+package poly.edu.contronller.Home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "Layout/Layout";   // tên file thường
+    }
+}
