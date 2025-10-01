8e064423cafb32710da4c827280cc81517d550c5
Thanh123s <haahaa1242@gmail.com>
2025-10-01 13:41:06 +0700

    cart

---
+++ Project_Java5-master/src/main/java/poly/edu/contronller/ContronllerApplication.java
@@ -0,0 +1,13 @@
+package poly.edu.contronller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContronllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContronllerApplication.class, args);
+	}
+
+}
