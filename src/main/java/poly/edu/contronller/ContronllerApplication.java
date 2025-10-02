84fef8899a786c60e5fd28d89f24ab9f60e5d095
Dương Đình Trác - Lab1 <duongdinhtrac0103@gmail.com>
2025-10-03 00:08:46 +0700

    update Project mới nhất

--- src/main/java/poly/edu/contronller/ContronllerApplication.java
+++ src/main/java/poly/edu/contronller/ContronllerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ContronllerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ContronllerApplication.class, args);
 	}
-
 }
